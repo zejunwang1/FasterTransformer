5a63f1864969cefb3c8239d8f26d7693ce4d13e5
zejunwang1 <wangzejunscut@126.com>
2026-05-25 16:46:02 +0800

    delete dropout

--- examples/pytorch/bert/utils/modeling_bert_v2.py
+++ examples/pytorch/bert/utils/modeling_bert_v2.py
@@ -147,7 +147,6 @@ class BertForSequenceClassification(BertPreTrainedModel):
         self.num_labels = config.num_labels
 
         self.bert = BertModel(config)
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.classifier = nn.Linear(config.hidden_size, self.config.num_labels)
 
         self.init_weights()
